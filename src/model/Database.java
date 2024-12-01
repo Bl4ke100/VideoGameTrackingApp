52ec809d40f8491a52ccf14a1f302846791feb8e
Bl4ke100 <Bl4ke100@gmail.com>
2024-12-01 16:42:56 +0530

    s

---
+++ src/model/Database.java
@@ -0,0 +1,26 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+
+public class Database {
+   private static Connection connection;
+
+    public static void createConnection() throws Exception {
+        if (connection == null) {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/sadproject", "root", "Blake25145#");
+        }
+    }
+
+    public static ResultSet executeSearch(String query) throws Exception {
+        createConnection();
+        return connection.createStatement().executeQuery(query);
+    }
+
+    public static Integer executeIUD(String query) throws Exception {
+        createConnection();
+        return connection.createStatement().executeUpdate(query);
+    } 
+}
